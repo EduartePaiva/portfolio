ba102629160c378dfa49667134bbaff921012a84
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-08 14:26:06 -0300

    projects section part 1

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -3,5 +3,9 @@ export default function SectionHeading({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>;
+    return (
+        <h2 className="text-center text-3xl font-medium capitalize mb-8">
+            {children}
+        </h2>
+    );
 }
