f144c3a46a227b3b8aa09637be8343be63d22681
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-08-19 17:31:15 -0300

    changed the resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -85,7 +85,7 @@ export default function Intro() {
                     <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
                 <a
-                    href="/resume.pdf"
+                    href="/resume_en.pdf"
                     download
                     className="group bg-white dark:bg-white/10 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
                 >
